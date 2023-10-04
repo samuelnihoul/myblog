e77f3e93439f5e6360e403bef81b3c0f89c5ba1d
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-10-04 13:45:30 +0200

    60

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 def starting_page(request):
     return render(request,'blog/index.html')
 def posts(request):
-    pass
-
+    return render(request, 'blog/all-posts.html')
 def post_detail(request):
     pass
